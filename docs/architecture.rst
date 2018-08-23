4e24a6380336f5f1be22eb8dc4bab8112a8a7873
Ha Bui <ha.bui@niteco.se>
2018-08-23 22:49:49 +0700

    Clone from master

---
+++ docs/architecture.rst
@@ -0,0 +1,19 @@
+Architecture
+============
+
+.. toctree::
+   :maxdepth: 1
+
+   architecture/money
+   architecture/products
+   architecture/thumbnails
+   architecture/stock
+   architecture/orders
+   architecture/i18n
+   architecture/translations
+   architecture/search
+   architecture/payments
+   architecture/settings
+   architecture/page
+   architecture/gdpr
+   architecture/graphql
---
+++ saleor/graphql/product/resolvers.py
@@ -0,0 +1,60 @@
+from django.db.models import Q
+
+from ...product import models
+from ...product.utils import products_with_details
+from ..utils import filter_by_query_param, get_node
+from .types import Category
+
+PRODUCT_SEARCH_FIELDS = ('name', 'description', 'category__name')
+CATEGORY_SEARCH_FIELDS = ('name', 'slug', 'description', 'parent__name')
+COLLECTION_SEARCH_FIELDS = ('name', 'slug')
+ATTRIBUTES_SEARCH_FIELDS = ('name', 'slug')
+
+
+def resolve_attributes(category_id, info, query):
+    queryset = models.ProductAttribute.objects.prefetch_related('values')
+    queryset = filter_by_query_param(queryset, query, ATTRIBUTES_SEARCH_FIELDS)
+    if category_id:
+        # Get attributes that are used with product types
+        # within the given category.
+        category = get_node(info, category_id, only_type=Category)
+        if category is None:
+            return queryset.none()
+        tree = category.get_descendants(include_self=True)
+        product_types = {
+            obj[0]
+            for obj in models.Product.objects.filter(
+                category__in=tree).values_list('product_type_id')}
+        queryset = queryset.filter(
+            Q(product_types__in=product_types)
+            | Q(product_variant_types__in=product_types))
+    return queryset.distinct()
+
+
+def resolve_categories(info, query, level=None):
+    queryset = models.Category.objects.all()
+    if level is not None:
+        queryset = queryset.filter(level=level)
+    queryset = filter_by_query_param(queryset, query, CATEGORY_SEARCH_FIELDS)
+    return queryset.distinct()
+
+
+def resolve_collections(info, query):
+    # FIXME: Return collections based on user after rebasing to master
+    queryset = models.Collection.objects.all()
+    queryset = filter_by_query_param(queryset, query, COLLECTION_SEARCH_FIELDS)
+    return queryset
+
+
+def resolve_products(info, category_id, query):
+    user = info.context.user
+    queryset = products_with_details(user=user).distinct()
+    queryset = filter_by_query_param(queryset, query, PRODUCT_SEARCH_FIELDS)
+    if category_id is not None:
+        category = get_node(info, category_id, only_type=Category)
+        return queryset.filter(category=category).distinct()
+    return queryset
+
+
+def resolve_product_types():
+    return models.ProductType.objects.all().distinct()
